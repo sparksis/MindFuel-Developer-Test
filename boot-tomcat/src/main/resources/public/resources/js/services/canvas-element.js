0707f2723818e52c5da714863afd39ba87df3b2c
Colton English <colton@sparksis.com>
2016-11-25 03:10:08 -0700

    fixed race condition for jquery/angular load

--- boot-tomcat/src/main/resources/public/resources/js/services/canvas-element.js
+++ boot-tomcat/src/main/resources/public/resources/js/services/canvas-element.js
@@ -2,25 +2,29 @@
  * Gives angular a injectable reference to the page's Canvas
  */
 angular.module('drawingApp').factory('Canvas', function() {
-	var sketch = $('#drawing').data('sketch');
+	var el = document.getElementById("drawing");
+	Object.defineProperty(el,'sketch',{
+		get: function(){
+			return $('#drawing').data('sketch');
+		}
+	});
 	var redo = [];
 	var redraw = function() {
-		sketch.redraw.call(sketch)
+		el.sketch.redraw.call(el.sketch)
 	};
-	el = document.getElementById("drawing");
 	el.undo = function() {
-		redo.push(sketch.actions.pop());
+		redo.push(el.sketch.actions.pop());
 		redraw();
 	};
 	el.redo = function() {
 		// TODO validate redo (check to see if the stack has changed, if so do
 		// not go permit redoing)
-		sketch.actions.push(redo.pop());
+		el.sketch.actions.push(redo.pop());
 		redraw();
 	};
 	el.clear = function(){
-		sketch.actions = [];
-		sketch.redraw();
+		el.sketch.actions = [];
+		el.sketch.redraw();
 	}
 	return el;
 });
