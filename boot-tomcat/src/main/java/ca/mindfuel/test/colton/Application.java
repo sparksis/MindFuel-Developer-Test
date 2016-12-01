f638b1ad87849a3c781d5b2336896901036c8dbf
Colton English <colton@sparksis.com>
2016-12-01 15:05:45 -0700

    upgradding spring starter to latest version to align with the documentation

--- boot-tomcat/src/main/java/ca/mindfuel/test/colton/Application.java
+++ boot-tomcat/src/main/java/ca/mindfuel/test/colton/Application.java
@@ -11,7 +11,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
