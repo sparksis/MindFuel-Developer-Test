eef3c30812cc17a6f653cd5c3547a474aef6dd9a
Colton English <colton@sparksis.com>
2016-12-02 19:39:31 -0700

    cleanup

--- boot-tomcat/src/main/java/ca/mindfuel/test/colton/service/UserRepository.java
+++ boot-tomcat/src/main/java/ca/mindfuel/test/colton/service/UserRepository.java
@@ -1,18 +1,12 @@
 package ca.mindfuel.test.colton.service;
 
 import javax.persistence.EntityManager;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
-import ca.mindfuel.test.colton.model.Image;
 import ca.mindfuel.test.colton.model.User;
-import ca.mindfuel.test.colton.model.User_;
 import ca.mindfuel.test.colton.model.UserSession;
 
 @Repository
@@ -39,7 +33,7 @@ public class UserRepository extends AbstractRepository<String, User> {
 	@Bean
 	@Scope("request")
 	public User currentUser(UserSession userSession) {
-		return selectByUsername(userSession.getUsername());
+		return selectById(userSession.getUsername()).get();
 	}
 
 	/**
