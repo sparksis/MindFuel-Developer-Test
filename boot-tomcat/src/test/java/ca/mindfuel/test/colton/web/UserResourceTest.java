ed8079f4d2d8de1cee4fec74e552c52b7033f9ca
Colton English <colton@sparksis.com>
2016-12-03 19:49:27 -0700

    added some meaningful functional tests

--- boot-tomcat/src/test/java/ca/mindfuel/test/colton/web/UserResourceTest.java
+++ boot-tomcat/src/test/java/ca/mindfuel/test/colton/web/UserResourceTest.java
@@ -2,6 +2,7 @@ package ca.mindfuel.test.colton.web;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,6 +17,7 @@ import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import ca.mindfuel.test.colton.Application;
+import ca.mindfuel.test.colton.model.User;
 import ca.mindfuel.test.colton.web.UserResource.UserWrapper;
 
 @RunWith(SpringRunner.class)
@@ -24,17 +26,19 @@ import ca.mindfuel.test.colton.web.UserResource.UserWrapper;
 public class UserResourceTest {
 
 	/**
-	 * builds a simple UserWrapper object.  Consumers are expected to set a unique username
+	 * builds a simple UserWrapper object. Consumers are expected to set a
+	 * unique username
+	 * 
 	 * @return
 	 */
-	public static UserWrapper buildTestUser(){
+	public static UserWrapper buildTestUser() {
 		UserWrapper user = new UserWrapper();
 		user.setActive(true);
 		user.setPassword("password");
-		user.getRolesWanted().add("USER");
+		user.getRolesWanted().add("ROLE_USER");
 		return user;
 	}
-	
+
 	@Autowired
 	private TestRestTemplate restTemplate;
 
@@ -42,17 +46,16 @@ public class UserResourceTest {
 	public void setup() {
 		restTemplate.withBasicAuth("admin", "admin");
 	}
-	
+
 	@Test
-	public void testCreateUserWithoutCredentials(){
+	public void testCreateUserWithoutCredentials() {
 		restTemplate.withBasicAuth("wrong", "wrong");
 		UserWrapper user = buildTestUser();
 		user.setUsername("testCreateUserWithoutCredentials");
-		
+
 		ResponseEntity<Void> body = this.restTemplate.postForEntity("/rest/users", user, Void.class);
 		assertThat(body.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
 	}
-	
 
 	@Test
 	public void testCreateUser() {
@@ -63,4 +66,38 @@ public class UserResourceTest {
 		assertThat(body.getStatusCode()).isEqualTo(HttpStatus.CREATED);
 	}
 
+	@Test
+	public void testCreateInactiveUser() {
+		final String name = "testCreateInactiveUser";
+		UserWrapper user = buildTestUser();
+		user.setUsername(name);
+		user.setActive(false);
+
+		ResponseEntity<Void> body = this.restTemplate.postForEntity("/rest/users", user, Void.class);
+		assertThat(body.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+		User createUser = this.restTemplate.getForObject("/rest/users/" + name, User.class);
+		assertThat(createUser.isActive() == false);
+	}
+
+	@Test
+	public void testQueryDoesNotReturnPassword() {
+		User[] users = this.restTemplate.getForObject("/rest/users/", User[].class);
+		assertThat(users.length).isGreaterThan(0);
+		assertThat(Arrays.stream(users).allMatch(it -> it.getPassword() == null)).isTrue();
+	}
+
+	@Test
+	public void testGetDoesNotReturnPassword() {
+		User user = this.restTemplate.getForObject("/rest/users/admin", User.class);
+		assertThat(user).isNotNull();
+		assertThat(user.getPassword()).isNull();
+	}
+
+	@Test
+	public void testUserNotFound(){
+		ResponseEntity<User> user = this.restTemplate.getForEntity("/rest/users/testUserNotFound", User.class);
+		assertThat(user.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+	}
+	
 }
