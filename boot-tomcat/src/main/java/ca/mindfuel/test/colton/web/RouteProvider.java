50e73bff6776effcfb37e5a2ca508362a7244319
Colton English <colton@sparksis.com>
2016-12-03 20:39:16 -0700

    csrf-token is now in the route provider rather than a js file

--- boot-tomcat/src/main/java/ca/mindfuel/test/colton/web/RouteProvider.java
+++ boot-tomcat/src/main/java/ca/mindfuel/test/colton/web/RouteProvider.java
@@ -3,6 +3,11 @@ package ca.mindfuel.test.colton.web;
 import java.io.IOException;
 import java.io.InputStream;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +22,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RouteProvider {
 
-	@RequestMapping(path = { "/", "/login", "admin", "logout" })
-	public byte[] reroute() throws IOException {
+	@RequestMapping(path = { "/", "/login", "/admin", "/logout" })
+	public byte[] reroute(HttpServletRequest request,HttpServletResponse response) throws IOException {
+		Cookie csrfCookie = new Cookie("X-CSRF-TOKEN", ((CsrfToken)request.getAttribute("_csrf")).getToken());
+		csrfCookie.setHttpOnly(false);
+		//TODO set an expiry in line with spring
+		response.addCookie(csrfCookie);
+		
 		try (InputStream stream = RouteProvider.class.getResourceAsStream("/public/index.html")) {
 			return StreamUtils.copyToByteArray(stream);
 		}
